7131e6b586c4730a5bb6e45f39b12ec048952245
Victor Maslov <nakilon@gmail.com>
2022-10-18 18:05:09 +0300

    now it's possible to make it even simpler

--- test.rb
+++ test.rb
@@ -4,7 +4,7 @@ minitest_cuprite "headless": "darwin" != Gem::Platform.local.os, timeout: 10
 require "minitest/hooks"
 describe :test do
   require "open3"
-  def f expectation, status, string
+  def f expectation, (string, status)
     assert status.exitstatus.zero?, string
     visit "file://#{File.expand_path __dir__}/temp.htm"
     t = evaluate_script "document.getElementsByTagName('canvas')[0].toDataURL()"
@@ -15,19 +15,16 @@ describe :test do
     File.unlink "temp.htm" if File.exist? "temp.htm"
   end
   it "fen" do   # this example requires third-party gem
-    string, status = Bundler.with_unbundled_env{ Open3.capture2e "bundle exec ruby main.rb 'r3r2k/p1n2pb1/3p3p/1ppP1qN1/4N3/P3P3/1PQ2PP1/R4K1R w - - 0 1' > ../../temp.htm", chdir: "examples/fen" }
-    f 42826, status, string
+    f 42826, Bundler.with_unbundled_env{ Open3.capture2e "bundle exec ruby main.rb 'r3r2k/p1n2pb1/3p3p/1ppP1qN1/4N3/P3P3/1PQ2PP1/R4K1R w - - 0 1' > ../../temp.htm", chdir: "examples/fen" }
   end
   it "dot strip plot" do
-    string, status = Open3.capture2e "ruby examples/WO2022104460A1/main.rb > temp.htm"
-    f 23174, status, string
+    f 23174, Open3.capture2e("ruby examples/WO2022104460A1/main.rb > temp.htm")
   end
   it "scatter plot" do
     unless File.exist? "all.tsv"
       require "open-uri"
       File.write "all.tsv", open("https://storage.yandexcloud.net/gems.nakilon.pro/p5rb/all.tsv", &:read)
     end
-    string, status = Open3.capture2e "ruby examples/moscow/main.rb all.tsv > temp.htm"
-    f 31053, status, string
+    f 31053, Open3.capture2e("ruby examples/moscow/main.rb all.tsv > temp.htm")
   end
 end
