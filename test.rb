78fe6db35f804924ecc517e2e7dcd060eed7a3b3
Victor Maslov <nakilon@gmail.com>
2022-12-08 19:50:28 +0300

    chess font is a bit off on alpine linux

--- test.rb
+++ test.rb
@@ -24,7 +24,7 @@ describe :test do
     end
 
   it "fen" do   # this example requires third-party gem
-      check 0, 0xff8900000000ffffff9181808000ffffffdfa04201fe0000ffd12000c0090000, base64(*Bundler.with_unbundled_env{ Open3.capture2e "bundle install >/dev/null && bundle exec ruby main.rb 'r3r2k/p1n2pb1/3p3p/1ppP1qN1/4N3/P3P3/1PQ2PP1/R4K1R w - - 0 1'", chdir: "examples/fen" })
+      check 11, 0xff8900000000ffffff9181808000ffffffdfa04201fe0000ffd12000c0090000, base64(*Bundler.with_unbundled_env{ Open3.capture2e "bundle install >/dev/null && bundle exec ruby main.rb 'r3r2k/p1n2pb1/3p3p/1ppP1qN1/4N3/P3P3/1PQ2PP1/R4K1R w - - 0 1'", chdir: "examples/fen" })
   end
   it "dot strip plot" do
       check 0, 0x1d5cdc0d1d0c1d9f5720a6fff2fe020180df0000df81c03f0080ffff0000ff7f, base64(*Open3.capture2e("ruby examples/WO2022104460A1/main.rb"))
@@ -37,7 +37,7 @@ describe :test do
       check 0, 0x2e0f3f2f9c983034009ef8fafcf870304f0f3020c102403c639e7c78f8020404, file(*Open3.capture2e("ruby examples/moscow/main.rb all.tsv"))
   end
     it "bar chart" do
-      check 0, 0xfd7cf00080fec23d7ffee0408340c2fffe0000d040ff388167008b80408080ff, base64(*Open3.capture2e("ruby examples/tg_export/main.rb"))
+      check 1, 0xfd7cf00080fec23d7ffee0408340c2fffe0000d040ff388167008b80408080ff, base64(*Open3.capture2e("ruby examples/tg_export/main.rb"))
     end
   end
 
